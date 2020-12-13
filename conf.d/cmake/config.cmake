574413ec741da6dfe1d973df3b4d96d8551ecccc
Marc-Antoine Riou <marc-antoine.riou@iot.bzh>
2020-12-13 12:54:32 +0100

    [config.xml] Change the entry point according to the binding name
    Signed-off-by: Marc-Antoine Riou <marc-antoine.riou@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -175,7 +175,7 @@ set(WIDGET_TYPE application/vnd.agl.service)
 # This is the file that will be executed, loaded,
 # at launch time by the application framework.
 #
-set(WIDGET_ENTRY_POINT lib/afb-signal-composer.so)
+set(WIDGET_ENTRY_POINT lib/redis-binding.so)
 
 # Optional dependencies order
 # ---------------------------
